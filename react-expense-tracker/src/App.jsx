dadc976bc3774839445242aeb7f9c53ef364109a
RTamine01 <amne0002@gmail.com>
2025-01-02 00:25:20 +0100

    establish foundational routing with Dashboard and error handling

--- react-expense-tracker/src/App.jsx
+++ react-expense-tracker/src/App.jsx
@@ -1,10 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard, { DashboardLoader } from "./pages/Dashboard";
+import Error from "./pages/Error";
 
+const router = createBrowserRouter([
+  {
+      path: "/",
+      element: <Dashboard />,
+      loader: DashboardLoader,
+      errorElement: <Error />,
+  },
+]);
 
 function App() {
-
   return (
     <div className="App">
-      expense tracker
+      <RouterProvider router={router} />
     </div>
   )
 }
