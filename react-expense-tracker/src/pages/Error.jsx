b10da3ac0dbc893421f0e64f9e2cb81d0a1171eb
RTamine01 <amne0002@gmail.com>
2025-01-04 00:12:27 +0100

    enhance error handling with navigational buttons for Go Back and Home

--- react-expense-tracker/src/pages/Error.jsx
+++ react-expense-tracker/src/pages/Error.jsx
@@ -1,7 +1,24 @@
+import { Link, useNavigate, useRouteError } from "react-router-dom";
+import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
+
 const Error = () => {
+    const error = useRouteError();
+    const navigate = useNavigate();
+
     return (
-        <div>
-            <h2>404</h2>
+        <div className="error">
+            <h1>There is a problem...!!</h1>
+            <p>{error.message || error.statusText}</p>
+            <div className="flex-md">
+                <button className="btn btn--dark" onClick={() => navigate(-1)}>
+                    <ArrowUturnLeftIcon width={20} />
+                    <span>Go Back</span>
+                </button>
+                <Link to="/" className="btn btn--dark">
+                    <HomeIcon width={20} />
+                    <span>Home</span>
+                </Link>
+            </div>
         </div>
     );
 };
