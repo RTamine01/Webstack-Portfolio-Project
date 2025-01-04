d9a023cd00634b657da46dcb957ff7b93c9457f1
RTamine01 <amne0002@gmail.com>
2025-01-04 17:14:10 +0100

    Implement budget creation logic with dynamic color generation

--- react-expense-tracker/src/components/AddBudgetForm.jsx
+++ react-expense-tracker/src/components/AddBudgetForm.jsx
@@ -27,6 +27,7 @@ const AddBudgetForm = () => {
                         required
                     />
                 </div>
+                <input type="hidden" name="_action" value="createBudget" />
                 <button type="submit" className="btn btn--dark">
                     <span>Create budget</span>
                     <CurrencyEuroIcon width={20} />
