517927c11cb1e9cece03d1044104b4420bef989c
RTamine01 <amne0002@gmail.com>
2025-01-04 01:47:35 +0100

    Add budget creation functionality with form and integration on dashboard

---
+++ react-expense-tracker/src/components/AddBudgetForm.jsx
@@ -0,0 +1,39 @@
+import { CurrencyEuroIcon } from "@heroicons/react/24/solid";
+import { Form } from "react-router-dom";
+
+const AddBudgetForm = () => {
+    return (
+        <div className="form-wrapper">
+            <h2 className="h3">Create Budget</h2>
+            <Form method="post" className="grid-sm">
+                <div className="grid-xs">
+                    <label htmlFor="newBudget">Budget Name</label>
+                    <input
+                        type="text"
+                        name="newBudget"
+                        id="newBudget"
+                        placeholder="e.g., Cosmetics"
+                        required
+                    />
+                </div>
+                <div className="grid-xs">
+                    <label htmlFor="newBudgetAmount">Amount</label>
+                    <input
+                        type="number"
+                        step="0.5"
+                        name="newBudgetAmount"
+                        id="newBudgetAmount"
+                        placeholder="$100"
+                        required
+                    />
+                </div>
+                <button type="submit" className="btn btn--dark">
+                    <span>Create budget</span>
+                    <CurrencyEuroIcon width={20} />
+                </button>
+            </Form>
+        </div>
+    );
+};
+
+export default AddBudgetForm;
--- react-expense-tracker/src/pages/Dashboard.jsx
+++ react-expense-tracker/src/pages/Dashboard.jsx
@@ -1,18 +1,47 @@
 import { useLoaderData } from "react-router-dom";
 import { fetchData } from "../helpers";
+import Intro from "../components/Intro";
+import { toast } from "react-toastify";
+import AddBudgetForm from "../components/AddBudgetForm";
 
 export function DashboardLoader() {
     const userName = fetchData("userName");
-    return { userName };
+    const budgets = fetchData("budgets");
+    return { userName, budgets };
+}
+
+export async function dashboardAction({ request }) {
+    const data = await request.formData();
+    const formData = Object.fromEntries(data);
+    try {
+        localStorage.setItem("userName", JSON.stringify(formData.userName));
+        return toast.success(`Welcome, ${formData.userName}`);
+    } catch (e) {
+        throw new Error("Problem creating your account.");
+    }
 }
 
 const Dashboard = () => {
-    const {userName} = useLoaderData()
+    const {userName, budgets} = useLoaderData()
     
   return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
+    <>
+        {userName ? ( 
+             <div className="dashboard">
+             <h1>
+                 Welcome, <span className="accent">{userName}</span>
+             </h1>
+             <div className="grid-sm">
+                <div className="grid-lg">
+                    <div className="flex-lg">
+                        <AddBudgetForm />
+                    </div>
+                </div>
+             </div>
+        </div>
+         ) : <Intro />}
+      
+    </>
   );
 }
 export default Dashboard;
