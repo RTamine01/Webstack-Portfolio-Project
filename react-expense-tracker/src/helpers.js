0073df6026213cef29467fdb876189bae0e8e12e
RTamine01 <amne0002@gmail.com>
2025-01-08 00:21:11 +0100

    Add utility functions for data retrieval and deletion in helpers.js
    - Implemented getAllMatchingItems to fetch and filter data from localStorage based on a specified key-value pair, enhancing data querying capabilities.
    - Added deleteItem function to handle both targeted item deletion by ID and full key removal from localStorage, improving flexibility in managing stored data.

--- react-expense-tracker/src/helpers.js
+++ react-expense-tracker/src/helpers.js
@@ -11,9 +11,6 @@ export const fetchData = (key) => {
     return JSON.parse(localStorage.getItem(key));
 };
 
-export const deleteItem = ({key}) => {
-    return localStorage.removeItem(key);
-};
 
 export const createBudget = ({ name, amount }) => {
     const newItem = {
@@ -62,3 +59,16 @@ export const formatCurrency = (amnt) => {
     });
 };
 
+export const getAllMatchingItems = ({ category, key, value }) => {
+    const data = fetchData(category) ?? [];
+    return data.filter((item) => item[key] === value);
+};
+
+export const deleteItem = ({key, id}) => {
+    const existingData = fetchData(key);
+    if(id){
+        const newData = existingData.filter((item) => item.id !== id);
+        return localStorage.setItem(key, JSON.stringify(newData));
+    }
+    return localStorage.removeItem(key);
+};
